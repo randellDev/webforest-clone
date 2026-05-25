19fd5a657b7caa76b404687f11fdae18e08e29f0
randellDev <randellgabatan13@gmail.com>
2026-05-26 03:32:47 +0800

    chore:creative-partner new background

---
+++ src/app/component/consultation/consultation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation',
+  imports: [],
+  templateUrl: './consultation.html',
+  styleUrl: './consultation.scss',
+})
+export class Consultation {}
